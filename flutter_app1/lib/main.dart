42fb7da812432aebd0c740ad3e2bb33ece93d45c
cyberpunk <cc1433@gmail.com>
2020-06-05 17:47:00 +0530

    Organizing code in Flutter app.

--- flutter_app1/lib/main.dart
+++ flutter_app1/lib/main.dart
@@ -1,21 +1,19 @@
 import "package:flutter/material.dart";
+import 'package:flutterapp1/app_screens/first_screen.dart';
 
-void main() {
-  runApp(MaterialApp(
-      title: "My Flutter App",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My First App Screen"),
-        ),
-        body: Material(
-          color: Colors.lightBlueAccent,
-          child: Center(
-            child: Text(
-              "Hello Flutter",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(color: Colors.white, fontSize: 40.0),
-            ),
-          ),
-        ),
-      )));
+void main() => runApp(new MyFlutterApp());
+
+class MyFlutterApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "My Flutter App",
+        home: Scaffold(
+            appBar: AppBar(title: Text("My First App Screen"),),
+            body: FirstScreen()
+        )
+    );
+  }
 }
