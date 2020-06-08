899ee31efd4bd4471a6c7c97f0182f94862bddef
cyberpunk <cc1433@gmail.com>
2020-06-08 16:48:19 +0530

    Row,Column and Expanded widget.

--- flutter_app1/lib/app_screens/home.dart
+++ flutter_app1/lib/app_screens/home.dart
@@ -7,22 +7,75 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
         child: Container(
-      alignment: Alignment.center,
-      color: Colors.deepPurple,
-      /*width: 200.0,
-      height: 100.0,
-      margin: EdgeInsets.only(left: 35.0,top:50.0 ),*/
-      child: Text(
-          "Flight",
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-              decoration: TextDecoration.none,
-              fontSize: 75,
-              fontFamily: 'Raleway',
-              fontWeight: FontWeight.w300,
-              color: Colors.white,
-          ),
-      ),
-    ));
+            padding: EdgeInsets.only(left: 10.0,top: 40.0),
+            alignment: Alignment.center,
+            color: Colors.deepPurple,
+            /*width: 200.0,
+              height: 100.0,
+              margin: EdgeInsets.only(left: 35.0,top:50.0 ),*/
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        "Indigo",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 35,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w300,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "Bangalore to Nagpur",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 20,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w300,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        "Air India",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 35 ,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w300,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "Bangalore to Goa",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 20,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w300,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            )));
   }
 }
