9af7d955df06cebe1831489453deb226d20cd6f0
cyberpunk <cc1433@gmail.com>
2020-06-10 11:03:24 +0530

    Long List View.

--- flutter_app1/lib/main.dart
+++ flutter_app1/lib/main.dart
@@ -6,35 +6,28 @@ void main(){
     MaterialApp(
       title: "Exploring UI Widget",
       home: Scaffold(
-        appBar: AppBar(title: Text("Basic List View"),),
+        appBar: AppBar(title: Text("Long List"),),
         body: getListView(),
       ),
     )
   );
 }
+List<String> getListElements(){
+  var items=List<String>.generate(1000, (counter)=>"Item : $counter");
+  return items;
+}
 Widget getListView(){
-  var listView=ListView(
-    children: <Widget>[
-      ListTile(
-        leading: Icon(Icons.landscape),
-        title: Text("Landscape"),
-        subtitle: Text("Beautiful View !"),
-        trailing: Icon(Icons.wb_sunny),
-        onTap: (){
-          debugPrint("Landscape Tapped");
-        },
-      ),
-      ListTile(
-        leading: Icon(Icons.laptop_chromebook),
-        title: Text("Windows"),
-      ),
-      ListTile(
-        leading: Icon(Icons.phone),
-        title: Text("Phone"),
-      ),
-      Text("Yet another element in list"),
-      Container(color: Colors.red,height: 50.0,)
-    ],
+  var listItems=getListElements();
+  var listView=ListView.builder(
+      itemBuilder: (context,index){
+        return ListTile(
+          leading: Icon(Icons.arrow_right),
+          title:Text(listItems[index]),
+          onTap: (){
+            debugPrint("${listItems[index]} was tapped");
+          },
+        );
+      }
   );
   return listView;
 }
